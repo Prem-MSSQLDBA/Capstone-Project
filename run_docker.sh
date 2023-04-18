068d16371530396b721fb3563dec9f9a5335ccbb
prem <mapremchandar@gmail.com>
2023-04-18 13:25:16 +0530

    Modifying run_docker

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build . -t udacity-capstone
+docker build . -t Prem-capstone
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-# docker run -p 80:80 udacity-capstone
+# docker run -p 80:80 Prem-capstone
